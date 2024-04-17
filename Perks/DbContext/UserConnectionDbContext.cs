1d0a5c71c7279a3cffc1ee80862d317f70807387
Ludo Tielbeke <ludotielbeke@gmail.com>
2024-04-17 20:01:10 +0200

    first commit

---
+++ Perks/DbContext/UserConnectionDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using OpenMod.API.Plugins;
+using OpenMod.EntityFrameworkCore;
+using OpenMod.EntityFrameworkCore.Configurator;
+using OpenMod.EntityFrameworkCore.MySql.Extensions;
+
+namespace Reinier.Perks.DbContext;
+
+public class UserConnectionDbContext: OpenModDbContext<UserConnectionDbContext>
+{
+    public UserConnectionDbContext(IServiceProvider serviceProvider) : base(serviceProvider)
+    {
+    }
+
+    public UserConnectionDbContext(IDbContextConfigurator configurator, IServiceProvider serviceProvider) : base(configurator, serviceProvider)
+    {
+    }
+    
+    
+}
+
+public class PluginContainerConfigurator : IPluginContainerConfigurator
+{
+    public void ConfigureContainer(IPluginServiceConfigurationContext context)
+    {
+        // You can extend how your database context works by using the overloads of this method.
+        context.ContainerBuilder.AddMySqlDbContext<UserConnectionDbContext>();
+    }
+}
